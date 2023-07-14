c3c075196a73fc54057a0ff1a1824fb7d988f6ba
prehmieren <prehm.soongnart@hotmail.com>
2023-07-15 04:25:36 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// // my methode
+function toBlack() {
+    document.getElementById("watch").src = "black-watch.png";
+    document.getElementById("watch").alt = "black-watch.png";
+}
+function toOrange() {
+    document.getElementById("watch").src = "orange-watch.png";
+    document.getElementById("watch").alt = "orange-watch.png";
+}
+function toPurple() {
+    document.getElementById("watch").src = "purple-watch.png";
+    document.getElementById("watch").alt = "purple-watch.png";
+}
+function toPink() {
+    document.getElementById("watch").src = "pink-watch.png";
+    document.getElementById("watch").alt = "pink-watch.png";
+}
+
+// //mentor's methode
+// var blackColor = document.getElementById("color1");
+// var redColor = document.getElementById("color2");
+// var purpleColor = document.getElementById("color3");
+// var pinkColor = document.getElementById("color4");
+// var watchImage = document.getElementById("watch");
+
+// blackColor.addEventListener('click', function () {
+//     watchImage.src = "black-watch.png";
+//     watchImage.alt = "smart-watch-black";
+// });
+
+// redColor.addEventListener('click', function () {
+//     watchImage.src = "orange-watch.png";
+//     watchImage.alt = "smart-watch-red";
+// });
+
+// purpleColor.addEventListener('click', function () {
+//     watchImage.src = "purple-watch.png";
+//     watchImage.alt = "smart-watch-purple";
+// });
+
+// pinkColor.addEventListener('click', function () {
+//     watchImage.src = "pink-watch.png";
+//     watchImage.alt = "smart-watch-pink";
+// });
+
